0e0010f9ba3141fe6865684f5369d10d26daaff5
Ivo Krugers <i.krugers@xablu.com>
2022-04-05 22:32:48 +0200

    initial

---
+++ VisualStudioMac.GetGitUrl/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace VisualStudioMac.GetGitUrl
+{
+    public static class Constants
+    {
+        public const string Version = "1.0";
+        public const string OneClickChar
+#if DEBUG
+         = "-->>";
+#else
+         = "-->";
+#endif
+        internal static string[] ExcludedExtensionsFromOneClick = { ".storyboard", ".xib", ".png", ".ttf" };
+    }
+}
---
+++ VisualStudioMac.GetGitUrl/Properties/AddinInfo.cs
@@ -0,0 +1,19 @@
+﻿using Mono.Addins;
+using Mono.Addins.Description;
+using VisualStudioMac.GetGitUrl;
+
+[assembly: Addin(
+    Id = "GetGitUrl",
+    Namespace = "VisualStudioMac",
+    Version = Constants.Version,
+    Category = "IDE extensions"
+)]
+
+[assembly: AddinName("Get Git Repo URL of selected lines")]
+[assembly: AddinDescription("This extension copies the URL of the selected lines in the connected Git Repo to the clipboard. GITHUB and Azure DevOps are supported.\n\nby Ivo Krugers")]
+[assembly: AddinAuthor("Ivo Krugers")]
+[assembly: AddinUrl("https://github.com/IvoKrugers")]
+
+[assembly: AddinDependency("::MonoDevelop.Core", MonoDevelop.BuildInfo.Version)]
+[assembly: AddinDependency("::MonoDevelop.Ide", MonoDevelop.BuildInfo.Version)]
+[assembly: AddinDependency("::MonoDevelop.TextEditor", MonoDevelop.BuildInfo.Version)]
